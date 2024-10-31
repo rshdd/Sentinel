15085668b246a67ca326b92135dce58939a0a4fa
Pablo Raschid <pablo.raschid@cerby.com>
2024-10-31 16:17:49 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TechSupportForm from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <TechSupportForm />
+    </div>
+  );
+}
+
+export default App;
